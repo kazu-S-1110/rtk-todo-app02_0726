837e293c5955a3a522c26b77b47d243451e827e1
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-07-28 06:27:20 +0900

    taskSliceを作成。

---
+++ src/features/task/taskSlice.ts
@@ -0,0 +1,55 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { RootState } from '../../app/store';
+
+export interface taskState {
+  idCount: number;
+  tasks: {
+    idCount: number;
+    title: string;
+    completed: boolean;
+  }[];
+  selectTask: {
+    id: number;
+    title: string;
+    completed: boolean;
+  };
+  isModalOpen: boolean;
+  filterSwitch: 'all' | 'fin' | 'no-fin';
+}
+
+const initialState: taskState = {
+  idCount: 0,
+  tasks: [],
+  selectTask: {
+    id: 0,
+    title: '',
+    completed: false,
+  },
+  isModalOpen: false,
+  filterSwitch: 'all',
+};
+
+// The function below is called a thunk and allows us to perform async logic. It
+// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
+// will call the thunk with the `dispatch` function as the first argument. Async
+// code can then be executed and other actions can be dispatched. Thunks are
+// typically used to make async requests.
+
+export const taskSlice = createSlice({
+  name: 'task',
+  initialState,
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {},
+});
+
+export const {} = taskSlice.actions;
+
+// The function below is called a selector and allows us to select a value from
+// the state. Selectors can also be defined inline where they're used instead of
+// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
+export const tasks = (state: RootState) => state.task.tasks;
+
+// We can also write thunks by hand, which may contain both sync and async logic.
+// Here's an example of conditionally dispatching actions based on current state.
+
+export default taskSlice.reducer;
